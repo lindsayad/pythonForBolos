0ab53fbe5fc73562598a5e799fa767c9b8484dae
Alex Lindsay <adlinds3@ncsu.edu>
2015-08-13 19:09:38 -0700

    I don't know what I changed.

--- Argon.py
+++ Argon.py
@@ -3,16 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as co
 from collections import OrderedDict
+import os
 
 # compute parameters needed for solver given common system inputs
-p = 1.01e5
-T = 300
+p = 133
+T = 400
 N = p/(co.k*T)
 xAr = 1.0
 # Set-up the electric field values we want to compute for
-EStart = 2.414323855084169554e+04
-EFin = 3.380053397117837518e+07
-nEf = 100 # number of Electric field nodes
+EStart = 1.0e+03
+EFin = 1.0e+06
+nEf = 200 # number of Electric field nodes
 mult = (EFin/EStart)**(1.0/(nEf - 1.0))
 EArray = np.zeros(nEf)
 mu = np.zeros(nEf)
@@ -43,26 +44,36 @@ for i in range(0,nEf):
     En = EField/N
     boltzmann.EN = En
     boltzmann.init()
-    fMaxwell = boltzmann.maxwell(2.0)
-    f = boltzmann.converge(fMaxwell,maxn=100,rtol=1e-5)
+    fMaxwell = boltzmann.maxwell(5.0)
+    if i==0:
+        f = boltzmann.converge(fMaxwell,maxn=100,rtol=1e-5)
+    else:
+        f = boltzmann.converge(f,maxn=100,rtol=1e-6)
     mean_energy[i] = boltzmann.mean_energy(f)
     mu[i] = boltzmann.mobility(f)/N
     Diff[i] = boltzmann.diffusion(f)/N
     kAr[i] = boltzmann.rate(f,"Ar -> Ar^+")
     alpha[i] = 1.0/(mu[i]*En)*(xAr*kAr[i])
-D_Path = '/mnt/Data/AlexApps/pythonForBolos/'
-t_vars = ['kAr','alpha']
-file_list = [D_Path + t_var + '_LFA.txt' for t_var in t_vars]
-file_list = OrderedDict.fromkeys(file_list)
-t_vars = OrderedDict.fromkeys(t_vars)
-# t_vars['mob'] = mu
-# t_vars['diff'] = Diff
-t_vars['alpha'] = alpha
-t_vars['kAr'] = kAr
-# t_vars['eta'] = eta
-# t_vars['elastic'] = elastic
-for f,t_var in zip(file_list.keys(),t_vars.keys()):
-    with open(f,'w') as write_file:
-        for i in range(0,nEf):
-            write_file.write('{0:.18e}, {1:.18e}\n'.format(EArray[i],t_vars[t_var][i]))
-    write_file.closed
+D_Path = os.path.expandvars('${ZAPDIR}/src/materials/')
+f = D_Path + "td_argon.txt"
+with open(f,'w') as write_file:
+    for i in range(0,nEf):
+        write_file.write('{0:.18e} {1:.18e} {2:.18e} {3:.18e}\n'.format(EArray[i], mu[i], Diff[i], alpha[i]))
+
+
+
+# t_vars = ['kAr','alpha']
+# file_list = [D_Path + t_var + '_LFA.txt' for t_var in t_vars]
+# file_list = OrderedDict.fromkeys(file_list)
+# t_vars = OrderedDict.fromkeys(t_vars)
+# # t_vars['mob'] = mu
+# # t_vars['diff'] = Diff
+# t_vars['alpha'] = alpha
+# t_vars['kAr'] = kAr
+# # t_vars['eta'] = eta
+# # t_vars['elastic'] = elastic
+# for f,t_var in zip(file_list.keys(),t_vars.keys()):
+#     with open(f,'w') as write_file:
+#         for i in range(0,nEf):
+#             write_file.write('{0:.18e}, {1:.18e}\n'.format(EArray[i],t_vars[t_var][i]))
+#     write_file.closed
